7eaeb804de199d4a97d7514a44c90cf367bacdb2
enlambdment <fivehmay@gmail.com>
2024-08-29 23:31:27 -0400

    finish ArrayUSet / ArrayBackedBag for test-driving USet / Bag traits respectively

--- impl/src/ArrayBackedBag.scala
+++ impl/src/ArrayBackedBag.scala
@@ -1,1 +1,27 @@
-case class ArrayBackedBag()
+package impl
+
+import cats.kernel.Eq
+import scala.reflect.ClassTag
+
+import api.{Bag, Partition}
+import impl.{RootishArrayStack, ArrayUSet}
+
+/**
+ * A simple ArrayUSet-backed implementation of a Bag (multiset.) Not meant to be efficient.
+ *
+ * @param classTag$A$0 Type of values contained in the bag.
+ * @param eqA There must be an instance of Eq[A].
+ * @tparam A Type of contents of the unordered bag (multiset.)
+ */
+class ArrayBackedBag[A: ClassTag](
+  implicit eqA: Eq[A]
+) extends Bag[A, RootishArrayStack[A], ArrayUSet[Partition[A, RootishArrayStack[A]]]] {
+  override val eq: Eq[A] = eqA
+  override var items: ArrayUSet[Partition[A, RootishArrayStack[A]]] = new ArrayUSet()
+  override val listFactory: RootishArrayStack[A] = new RootishArrayStack()
+  override val iterator: Iterator[A] = for {
+    partition <- items.iterator
+    ms = partition.members
+    m <- ms.toScalaListBuffer
+  } yield { m }
+}
--- impl/src/ArrayUSet.scala
+++ impl/src/ArrayUSet.scala
@@ -1,3 +1,61 @@
-class ArrayUSet {
+package impl
 
+import cats.kernel.Eq
+
+import scala.reflect.ClassTag
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+/**
+ * A simple list-backed implementation of a USet (unordered set.) Merely for test-driving
+ * so I know how it plays with the cats typeclass Eq (and can check that Bag does the right thing.)
+ *
+ * This is about as bad as it gets efficiency-wise: adding an arbitrary element
+ * is O(n) worst-case in n := this.size(), because we have to check the entire list
+ * to make sure no equal values are already present. And since we don't maintain a
+ * sorted backing list, there's no way to do this without confirming that every single
+ * value in the backing list doesn't equal the one we're trying to add.
+ *
+ * @param classTag$A$0  Type of values contained in the unordered set.
+ * @param eqA There must be an instance of Eq[A] (i.e. the members of type A must be
+ *            comparable in a defined sense) in order to build ArrayUSet's representing
+ *            an unordered set of values in A
+ * @tparam A  Type of contents of the unordered set.
+ */
+class ArrayUSet[A: ClassTag](implicit eqA: Eq[A]) extends api.USet[A] {
+  override implicit val eq: Eq[A] = eqA
+
+  private val xs: mutable.ListBuffer[A] = new ListBuffer[A]
+
+  override def iterator: Iterator[A] = xs.iterator
+
+  override def size(): Int = xs.length
+
+  override def add(x: A): Boolean = {
+    val notPresent = !xs.contains(x)
+    if (notPresent) {
+      xs.insert(xs.length, x)
+    }
+    notPresent
+  }
+
+  override def remove(x: A): Option[A] = {
+    val foundAt: Int = xs.indexWhere(eq.eqv(x, _))
+    val result: Option[A] = if (foundAt > -1) {
+      Some(xs.remove(foundAt))
+    } else {
+      None
+    }
+    result
+  }
+
+  override def find(x: A): Option[A] = {
+    val foundAt: Int = xs.indexWhere(eq.eqv(x, _))
+    val result: Option[A] = if (foundAt > -1) {
+      Some(xs(foundAt))
+    } else {
+      None
+    }
+    result
+  }
 }
